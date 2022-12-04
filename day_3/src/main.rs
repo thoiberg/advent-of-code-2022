416f670e0c4b40d90537e95f6db4d0dd3949de6e
Tim Hoiberg <tim.hoiberg@gmail.com>
2022-12-04 20:41:53 +0900

    Add Initial Part One solution

---
+++ day_3/src/main.rs
@@ -0,0 +1,99 @@
+use std::collections::HashSet;
+
+fn main() {
+    let data = process_data(read_data());
+    let part_one_answer = part_one_solution(&data);
+
+    println!("Part One Solution is: {}", part_one_answer);
+}
+
+fn part_one_solution(rucksacks: &Vec<Rucksack>) -> u32 {
+    rucksacks
+        .into_iter()
+        .fold(0, |acc, rucksack| acc + rucksack.total_value())
+}
+
+struct Rucksack {
+    left_compartment: HashSet<char>,
+    right_compartment: HashSet<char>,
+}
+
+impl Rucksack {
+    pub fn shared_chars(&self) -> Vec<&char> {
+        self.left_compartment
+            .intersection(&self.right_compartment)
+            .collect::<Vec<&char>>()
+            .clone()
+    }
+
+    pub fn total_value(&self) -> u32 {
+        self.shared_chars()
+            .iter()
+            .fold(0, |acc, shared_char| acc + self.value_for(shared_char))
+    }
+
+    fn value_for(&self, char: &char) -> u32 {
+        let lower_case = ('a'..='z').collect::<Vec<char>>();
+        let upper_case = ('A'..='Z').collect::<Vec<char>>();
+
+        match lower_case.iter().position(|&x| &x == char) {
+            Some(idx) => {
+                // get the index (+ 1) and return
+                (idx + 1).try_into().unwrap()
+            }
+            None => {
+                // get the position from the upper_case range, and plus +27
+                match upper_case.iter().position(|&x| &x == char) {
+                    Some(idx) => (idx + 27).try_into().unwrap(),
+                    None => panic!(
+                        "Didn't find the character in either set, this shouldn't have happened..."
+                    ),
+                }
+            }
+        }
+    }
+}
+
+fn read_data() -> &'static str {
+    include_str!("../data/puzzle_data")
+}
+
+fn process_data(data: &str) -> Vec<Rucksack> {
+    data.split("\n")
+        .into_iter()
+        .map(|rucksack| {
+            let chars: Vec<char> = rucksack.chars().collect();
+            let halfway_point = chars.len() / 2;
+            let left_compartment = chars[0..halfway_point].to_vec().into_iter().collect();
+            let right_compartment = chars[halfway_point..].to_vec().into_iter().collect();
+
+            Rucksack {
+                left_compartment,
+                right_compartment,
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod test_super {
+    use super::*;
+
+    fn test_data() -> Vec<Rucksack> {
+        process_data(include_str!("../data/example_data"))
+    }
+
+    #[test]
+    fn test_part_one_example() {
+        let answer = part_one_solution(&test_data());
+
+        assert_eq!(answer, 157);
+    }
+
+    #[test]
+    fn test_part_one_solution() {
+        let answer = part_one_solution(&process_data(read_data()));
+
+        assert_eq!(answer, 8123);
+    }
+}
