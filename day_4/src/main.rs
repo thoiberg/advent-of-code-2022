bc9d2740d3c093576792e16219f8a9e837234e1a
Tim Hoiberg <tim.hoiberg@gmail.com>
2022-12-05 23:34:56 +0900

    Add initial part one solution

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,65 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let data = process_data(&read_data());
+
+    let part_one_answer = part_one_solution(&data);
+    println!("Part One Solution is: {}", part_one_answer);
+}
+
+fn part_one_solution(elf_pairs: &Vec<Vec<HashSet<u32>>>) -> u32 {
+    elf_pairs.into_iter().fold(0, |acc, elf_pair| {
+        let left_side = &elf_pair[0];
+        let right_side = &elf_pair[1];
+
+        if left_side.is_subset(&right_side) || left_side.is_superset(&right_side) {
+            acc + 1
+        } else {
+            acc
+        }
+    })
+}
+
+fn process_data(data: &str) -> Vec<Vec<HashSet<u32>>> {
+    data.split("\n")
+        .into_iter()
+        .map(|elf_pair| {
+            elf_pair
+                .split(",")
+                .into_iter()
+                .map(|assignment| {
+                    let range_bookends: Vec<u32> = assignment
+                        .split("-")
+                        .into_iter()
+                        .map(|range_bookend| range_bookend.parse::<u32>().unwrap())
+                        .collect();
+
+                    let start = range_bookends[0];
+                    let end = range_bookends[1];
+
+                    (start..=end).collect::<Vec<u32>>().into_iter().collect()
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn read_data() -> &'static str {
+    include_str!("../data/puzzle_data")
+}
+
+#[cfg(test)]
+mod test_super {
+    use super::*;
+
+    fn test_data() -> Vec<Vec<HashSet<u32>>> {
+        process_data(include_str!("../data/example_data"))
+    }
+
+    #[test]
+    fn test_part_one_example() {
+        let answer = part_one_solution(&test_data());
+
+        assert_eq!(answer, 2);
+    }
 }
