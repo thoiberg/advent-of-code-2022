d806597c1213d13ae33b20792ff35fa49dd2743d
Tim Hoiberg <tim.hoiberg@gmail.com>
2022-12-08 12:46:59 +0900

    Giving up on day 7

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,5 +1,75 @@
+use std::{
+    borrow::Borrow,
+    cell::{Ref, RefCell},
+    rc::Rc,
+};
+
 fn main() {
-    println!("Hello, world!");
+    process_commands(read_data());
+}
+
+// install nom
+// parse through the command list
+// lines that start with $ are cmds
+// everything else is output
+// when cd set the working_dir to directory
+// when ls add all the files and directories to cwd
+
+fn process_commands(data: &str) {
+    let mut root = Rc::new(RefCell::new(Directory {
+        name: String::from("/"),
+        files: vec![],
+        directories: vec![],
+        parent: None,
+    }));
+    let mut node = root.clone();
+
+    for line in data.lines() {
+        let mut stroop = line.split(' ');
+        let (Some("$"), Some("cd"), Some(directory_name), None) = (stroop.next(), stroop.next(), stroop.next(), stroop.next()) else {
+            panic!("aaaaaaaaaaaaaaaah");
+        };
+        if directory_name == ".." {
+            let parent = node.borrow_mut().parent.clone().unwrap();
+            node = parent;
+        } else {
+            let child = node
+                .borrow_mut()
+                .directories
+                .push(Rc::new(RefCell::new(Directory {
+                    name: String::from("/"),
+                    files: vec![],
+                    directories: vec![],
+                    parent: Some(node),
+                })))
+                .clone();
+            // node = child;
+        }
+    }
+
+    // how to use nom
+    // set up an alt to either return command or output
+    // but I need a common type they both belong to??
+}
+
+fn read_data() -> &'static str {
+    include_str!("../data/example_data")
+}
+
+type NodeHandle = Rc<RefCell<Directory>>;
+
+#[derive(Clone, Default, Debug)]
+struct Directory {
+    name: String,
+    files: Vec<File>,
+    directories: Vec<NodeHandle>,
+    parent: Option<NodeHandle>,
+}
+
+#[derive(Clone, Debug)]
+struct File {
+    name: String,
+    size: u64,
 }
 
 #[cfg(test)]
