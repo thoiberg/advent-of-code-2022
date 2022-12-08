870a61f8aacdc7a57edac951358bd199d21b7a95
Tim Hoiberg <tim.hoiberg@gmail.com>
2022-12-09 01:05:43 +0900

    Initial solution of Part 8

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -5,6 +5,9 @@ fn main() {
 
     let part_one_solution = part_one_solution(&trees);
     println!("Part One Answer is: {}", part_one_solution);
+
+    let part_two_solution = part_two_solution(&trees);
+    println!("Part Two Answer is: {}", part_two_solution.unwrap());
 }
 
 fn part_one_solution(trees: &Array2<u32>) -> u32 {
@@ -28,6 +31,72 @@ fn part_one_solution(trees: &Array2<u32>) -> u32 {
             return acc;
         }
     })
+}
+
+fn part_two_solution(trees: &Array2<u32>) -> Option<u32> {
+    let mut scenic_score: Vec<u32> = vec![];
+
+    trees.indexed_iter().for_each(|((row, col), tree)| {
+        let mut above_done = false;
+        let above_score = trees.slice(s![0..row, col]).iter().rev().fold(0, |acc, x| {
+            if !above_done {
+                if tree > x {
+                    acc + 1
+                } else {
+                    above_done = true;
+                    acc + 1
+                }
+            } else {
+                acc
+            }
+        });
+        let mut below_done = false;
+        let below_score = trees.slice(s![row + 1.., col]).iter().fold(0, |acc, x| {
+            if !below_done {
+                if tree > x {
+                    acc + 1
+                } else {
+                    below_done = true;
+                    acc + 1
+                }
+            } else {
+                acc
+            }
+        });
+
+        let mut left_done = false;
+        let left_score = trees.slice(s![row, 0..col]).iter().rev().fold(0, |acc, x| {
+            if !left_done {
+                if tree > x {
+                    acc + 1
+                } else {
+                    left_done = true;
+                    acc + 1
+                }
+            } else {
+                acc
+            }
+        });
+        let mut right_done = false;
+        let right_score = trees.slice(s![row, col + 1..]).iter().fold(0, |acc, x| {
+            if !right_done {
+                if tree > x {
+                    acc + 1
+                } else {
+                    right_done = true;
+                    acc + 1
+                }
+            } else {
+                acc
+            }
+        });
+
+        if row == 3 && col == 2 {}
+
+        scenic_score.push(above_score * left_score * below_score * right_score);
+    });
+
+    scenic_score.into_iter().max()
 }
 
 fn read_data() -> &'static str {
@@ -69,5 +138,23 @@ mod test_super {
         let answer = part_one_solution(&data);
 
         assert_eq!(answer, 1789);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let data = process_data(include_str!("../data/example_data"));
+
+        let answer = part_two_solution(&data);
+
+        assert_eq!(answer, Some(8));
+    }
+
+    #[test]
+    fn test_part_two_solution() {
+        let data = process_data(read_data());
+
+        let answer = part_two_solution(&data);
+
+        assert_eq!(answer, Some(314820));
     }
 }
